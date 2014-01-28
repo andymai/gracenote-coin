04c1264e1886f565958a9a805327c98b008ff6f1
Andy Mai <hi@andymai.com>
2014-01-28 13:38:35 -0800

    2m block time

--- src/main.cpp
+++ src/main.cpp
@@ -834,7 +834,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 }
 
 static const int64 nTargetTimespan = 1 * 24 * 60 * 60; // gcoin: 1 days
-static const int64 nTargetSpacing = 15; // gcoin: 15s blocks
+static const int64 nTargetSpacing = 120; // gcoin: 2m blocks
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 // Thanks: Balthazar for suggesting the following fix
